42175578f6686c1b1089702c7aa80d1c5b4adf85
michaelp <michaelp@linux-laptop.lan>
2023-12-31 17:35:25 +0000

    fixed API problem

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphql1f874fb7": {
       "AppClientID": "string",
